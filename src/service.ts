88fed3b097911b73a8343fd1bf37515127885dec
dan-schel <me@danschellekens.com>
2024-09-28 21:44:40 +1000

    status icon + sentence/title casing

--- src/service.ts
+++ src/service.ts
@@ -1,5 +1,12 @@
 import { env } from "./env";
 
+// How many successful attempts must occur before a recent failure is no longer
+// reflected in the status.
+const flakyWindow = 5;
+
+const outdatedAfterLifecycles = 1.1;
+const deadAfterLifecycles = 3.1;
+
 export abstract class DataService {
   abstract init(): Promise<void>;
   abstract onListening(): void;
@@ -11,6 +18,7 @@ export abstract class PollingDataService extends DataService {
   private _succeededAt: Date | null = null;
   private _modifiedAt: Date | null = null;
   private _hash: string | null = null;
+  private _flakyCountdown: number = 0;
 
   constructor(private _dataName: string, private _pollMillis: number) {
     super();
@@ -22,6 +30,7 @@ export abstract class PollingDataService extends DataService {
     this._attemptedAt = new Date();
     this._hash = await this._downloadData();
     this._succeededAt = new Date();
+    this._flakyCountdown = 0;
 
     // This is our first data. We don't know.
     this._modifiedAt = null;
@@ -35,6 +44,7 @@ export abstract class PollingDataService extends DataService {
 
   override getStatus(): object {
     return {
+      status: this._calculateStatus(),
       url: env.URL + this._getUrl(),
       hash: this._hash,
       attemptedAt: this._attemptedAt?.toISOString() ?? null,
@@ -42,6 +52,25 @@ export abstract class PollingDataService extends DataService {
       modifiedAt: this._modifiedAt?.toISOString() ?? null,
       ...this._getAdditionalStatus(),
     };
+  }
+
+  private _calculateStatus() {
+    if (this._succeededAt == null) {
+      return "dead";
+    }
+
+    const lifecycles =
+      (Date.now() - this._succeededAt.getTime()) / this._pollMillis;
+
+    if (lifecycles > deadAfterLifecycles) {
+      return "dead";
+    } else if (lifecycles > outdatedAfterLifecycles) {
+      return "outdated";
+    } else if (this._flakyCountdown > 0) {
+      return "flaky";
+    } else {
+      return "healthy";
+    }
   }
 
   private async _refreshData() {
@@ -55,9 +84,15 @@ export abstract class PollingDataService extends DataService {
         this._modifiedAt = new Date();
       }
       this._hash = newHash;
+
+      if (this._flakyCountdown > 0) {
+        this._flakyCountdown--;
+      }
     } catch (err) {
       console.warn(`Failed to refresh ${this._dataName}. Retaining old data.`);
       console.warn(err);
+
+      this._flakyCountdown = flakyWindow;
     }
   }
 
