f21ed1dcb8c1e302edfe29d16e309291f2bdc968
dan-schel <me@danschellekens.com>
2024-10-14 21:12:13 +1100

    overrides

--- src/ptv-platforms/index.ts
+++ src/ptv-platforms/index.ts
@@ -3,67 +3,9 @@ import { DataService } from "../service";
 import { KnownPlatform, prepareDataFolder } from "../utils";
 import fsp from "fs/promises";
 import { fetchFromPtvApi } from "./fetch-platforms";
+import { stops } from "./stops";
 
 const dataFile = "data/ptv-platforms.json";
-
-const ptvStopCodes = [
-  // Central stations - Should always be fetched first.
-  1071, // Flinders Street
-  1181, // Southern Cross
-  1162, // Richmond
-  1144, // North Melbourne
-  1030, // Burnley
-
-  // Stations where platforms are unpredictable.
-  1012, // Auburn
-  1018, // Belgrave
-  1020, // Bentleigh
-  1026, // Box Hill
-  1032, // Camberwell
-  1037, // Chatham
-  1039, // Cheltenham
-  1044, // Craigieburn
-  1045, // Cranbourne
-  1049, // Dandenong
-  1054, // Diamond Creek
-  1057, // East Camberwell
-  1230, // East Pakenham
-  1062, // Eltham
-  1063, // Epping
-  1064, // Essendon
-  1073, // Frankston
-  1081, // Glen Huntly
-  1078, // Glen Waverley
-  1084, // Greensborough
-  1090, // Hawthorn
-  1113, // Laverton
-  1115, // Lilydale
-  1119, // McKinnon
-  1228, // Mernda
-  1132, // Moorabbin
-  1152, // Ormond
-  1157, // Patterson
-  1163, // Ringwood
-  1224, // South Morang
-  1187, // Sunbury
-  1229, // Union
-  1202, // Watergardens
-  1205, // Werribee
-  1208, // Westall
-
-  // Usually predictable, but not when disrupted.
-  1021, // Berwick
-  1036, // Caulfield
-  1041, // Clifton Hill
-  1051, // Darling
-  1072, // Footscray
-  1093, // Heidelberg
-  1134, // Mordialloc
-  1141, // Newport
-  1150, // Oakleigh
-  1161, // Reservoir
-  1218, // Sunshine
-];
 
 export class PtvPlatformsDataService extends DataService {
   private readonly _initialFetchInterval;
@@ -80,7 +22,7 @@ export class PtvPlatformsDataService extends DataService {
     this._regularFetchInterval = env.PTV_PLATFORMS_REGULAR_FETCH_SECONDS * 1000;
     this._stopIndex = 0;
     this._knownPlatforms = new Map();
-    this._status = new Map(ptvStopCodes.map((x) => [x, "pending"]));
+    this._status = new Map(stops.map((x) => [x, "pending"]));
     this._succeededAt = null;
     this._failedAt = null;
   }
@@ -116,8 +58,8 @@ export class PtvPlatformsDataService extends DataService {
       this._scheduleNextFetch();
     }
 
-    const stopID = ptvStopCodes[this._stopIndex];
-    this._stopIndex = (this._stopIndex + 1) % ptvStopCodes.length;
+    const stopID = stops[this._stopIndex];
+    this._stopIndex = (this._stopIndex + 1) % stops.length;
 
     try {
       const platforms = await fetchFromPtvApi(stopID);
