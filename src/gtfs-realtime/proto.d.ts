7b42fe9054f091828a905c7bb390a6709bfae7bd
dan-schel <me@danschellekens.com>
2025-04-25 13:33:17 +1000

    fix lint issues in auto generated files

--- src/gtfs-realtime/proto.d.ts
+++ src/gtfs-realtime/proto.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-require-imports */
 import * as $protobuf from "protobufjs";
 import Long = require("long");
 /** Namespace transit_realtime. */
