a870c4a85eb7eb9eeb330ef9d6a6b043d458cf8a
dan-schel <me@danschellekens.com>
2025-04-25 12:15:49 +1000

    refactor

---
+++ src/ptv-disruption-details/cache.ts
@@ -0,0 +1,29 @@
+export class Cache<T extends {}> {
+  private readonly _cache: Map<string, { data: T; expiry: Date }>;
+
+  constructor(private readonly _durationMs: number) {
+    this._cache = new Map();
+  }
+
+  get(key: string): T | null {
+    const entry = this._cache.get(key);
+    if (entry == null || entry.expiry < new Date()) {
+      return null;
+    }
+    return entry.data;
+  }
+
+  save(key: string, data: T) {
+    const expiry = new Date(Date.now() + this._durationMs);
+    this._cache.set(key, { data, expiry });
+    this.clean();
+  }
+
+  clean() {
+    for (const [key, value] of Array.from(this._cache.entries())) {
+      if (value.expiry < new Date()) {
+        this._cache.delete(key);
+      }
+    }
+  }
+}
--- src/ptv-disruption-details/fetch-details.ts
+++ src/ptv-disruption-details/fetch-details.ts
@@ -1,4 +1,4 @@
-import { parse } from "node-html-parser";
+import { parse, HTMLElement } from "node-html-parser";
 import { z } from "zod";
 
 const query = ".js-react[data-name='LiveTravelUpdatesPage_article']";
@@ -6,25 +6,54 @@ const schema = z.object({
   content: z.string(),
 });
 
-export async function fetchDetails(url: string): Promise<string> {
-  const response = await fetch(url);
-  const htmlStr = await response.text();
-  const html = parse(htmlStr);
+export type FetchDetailsResult =
+  | { details: string }
+  | { error: "parsing-error"; parsingError: string }
+  | { error: "fetch-failed" | "not-found" };
 
-  const element = html.querySelector(query);
+export async function fetchDetails(url: string): Promise<FetchDetailsResult> {
+  const fetchResult = await fetchHtml(url);
+  if (fetchResult == null) return { error: "fetch-failed" };
+  if ("notFound" in fetchResult) return { error: "not-found" };
+
+  const element = fetchResult.html.querySelector(query);
   if (element == null) {
-    throw new Error(`Nothing matches "${query}" query.`);
+    return {
+      error: "parsing-error",
+      parsingError: `Nothing matches "${query}" query.`,
+    };
   }
 
   const text = element.getAttribute("data-init-props") ?? null;
   if (text == null) {
-    throw new Error(`Element missing "data-init-props" attribute.`);
+    return {
+      error: "parsing-error",
+      parsingError: `Element missing "data-init-props" attribute.`,
+    };
   }
 
   try {
-    const json = JSON.parse(text);
-    return schema.parse(json).content;
+    const json = schema.parse(JSON.parse(text));
+    return { details: json.content };
   } catch (err) {
-    throw new Error(`Content of "data-init-props" is not the expected JSON.`);
+    return {
+      error: "parsing-error",
+      parsingError: `Content of "data-init-props" is not the expected JSON.`,
+    };
+  }
+}
+
+type FetchHtmlResult = { notFound: true } | { html: HTMLElement } | null;
+
+async function fetchHtml(url: string): Promise<FetchHtmlResult> {
+  try {
+    const response = await fetch(url);
+    if (response.status === 404) return { notFound: true };
+
+    const htmlStr = await response.text();
+    const html = parse(htmlStr);
+    return { html };
+  } catch {
+    return null;
   }
 }
